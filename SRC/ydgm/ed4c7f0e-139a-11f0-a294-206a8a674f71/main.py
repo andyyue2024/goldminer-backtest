e8dfbe19f7334fcfe67dfe1b9d1f0bd0c5074d49
Andy <yyhere_123@126.com>
2025-09-06 15:04:13 +0800

    pnl_ratio_annual: 19.21

--- SRC/ydgm/ed4c7f0e-139a-11f0-a294-206a8a674f71/main.py
+++ SRC/ydgm/ed4c7f0e-139a-11f0-a294-206a8a674f71/main.py
@@ -354,7 +354,7 @@ class AILabxTool:
 
 
 class AILabxStrategy:
-    def __init__(self, context, white_list: list = None, max_count: int = 1, w_aa=0.2, w_bb=1.5, w_cc=1, w_dd=0.16):
+    def __init__(self, context, white_list: list = None, max_count: int = 1, w_aa=0.2, w_bb=1.5, w_cc=1, w_dd=0.18):
         self.now = None
         self.context = context
         self.white_list = list(white_list)
@@ -629,7 +629,7 @@ if __name__ == '__main__':
         mode=MODE_BACKTEST,
         token='c8bd4de742240da9483aecd05a2f5e52900786eb',
         backtest_start_time="2024-01-09 09:30:00",
-        backtest_end_time='2025-08-21 15:00:00',
+        backtest_end_time='2025-09-08 15:00:00',
         # backtest_end_time='2023-10-20 15:00:00',
         backtest_adjust=ADJUST_NONE,
         backtest_initial_cash=100000,
